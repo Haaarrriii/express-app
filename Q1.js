67bae45e6e5593e7a3010716b6f6477e57644adc
Harwinder singh mann <161780459+Haaarrriii@users.noreply.github.com>
2024-08-11 23:00:46 -0400

    Update Q1.js

--- Q1.js
+++ Q1.js
@@ -76,6 +76,7 @@ app.post('/find4Digits', (req, res) => {
     res.send(`First Four-Digit Number: ${result}`);
 });
 
+const port = process.env.PORT||3000;
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
